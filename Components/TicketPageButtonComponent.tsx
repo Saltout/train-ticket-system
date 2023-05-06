2102b746115786f950ae6ddbe762d62e67403ddb
Saltout <93079332+Saltout@users.noreply.github.com>
2023-05-06 19:05:58 +0200

    1 file modified

--- Components/TicketPageButtonComponent.tsx
+++ Components/TicketPageButtonComponent.tsx
@@ -1,7 +1,12 @@
 import * as React from 'react';
+import './Styles/ButtonStyle.css';
 
 function TicketPageButton(props) {
-  return <button onClick={props.onClick}>{props.label}</button>;
+  return (
+    <button className="ticketPageButton" onClick={props.onClick}>
+      {props.label}
+    </button>
+  );
 }
 
 export default TicketPageButton;
